fe087d687086884ddc9d9508cacac8ce00b4323e
Melinna <melinna.art@gmail.com>
2024-02-03 12:44:35 -0500

    add logic for each button animation and sound onClick

--- app.js
+++ app.js
@@ -1,17 +1,59 @@
-$('button').on('click', () => {
-    console.log(gamePattern)
-})
 
 let gamePattern = []
 
 const buttonColors = ["red", "blue", "green", "yellow"]
 
-const nextSequence = () => {
-    const randomNum = Math.trunc(Math.random() * 3) + 1
+
+const randomChosenColorF = () => {
+    const randomNum = Math.trunc(Math.random() * 4)
+    console.log(randomNum)
     const randomChosenColor = buttonColors[randomNum]
-    console.log(randomChosenColor)
-    return gamePattern.push(randomChosenColor)
+    return randomChosenColor
 }
 
+
+const nextSequence = () => {
+    let colorToPush = randomChosenColorF()
+    return gamePattern.push(colorToPush)
+}
+
+randomChosenColorF()
+
+
+$(`.red`).on('click', () => {
+    if (randomChosenColorF() == buttonColors[0]) {
+        $('.red').fadeOut().fadeIn()
+        let audio = new Audio(`./sounds/red.mp3`)
+        audio.play()
+    }
+    console.log($(`button.red`))
+})
+
+$(`.blue`).on('click', () => {
+    if (randomChosenColorF() == buttonColors[1]) {
+        $('.blue').fadeOut().fadeIn()
+        let audio = new Audio(`./sounds/blue.mp3`)
+        audio.play()
+    }
+    console.log($(`button.blue`))
+})
+
+$(`.green`).on('click', () => {
+    if (randomChosenColorF() == buttonColors[2]) {
+        $('.green').fadeOut().fadeIn()
+        let audio = new Audio(`./sounds/green.mp3`)
+        audio.play()
+    }
+    console.log($(`button.green`))
+})
+
+$(`.yellow`).on('click', () => {
+    if (randomChosenColorF() == buttonColors[3]) {
+        $('.yellow').fadeOut().fadeIn()
+        let audio = new Audio(`./sounds/yellow.mp3`)
+        audio.play()
+    }
+    console.log($(`button.yellow`))
+})
 
 
